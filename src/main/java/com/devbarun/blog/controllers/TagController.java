35e05f1f37fec835cca55058863099cad0800ba6
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2025-04-17 18:12:33 +0545

    Tags endpoint added

---
+++ src/main/java/com/devbarun/blog/controllers/TagController.java
@@ -0,0 +1,47 @@
+package com.devbarun.blog.controllers;
+
+import com.devbarun.blog.domian.dtos.CreateTagsRequest;
+import com.devbarun.blog.domian.dtos.TagResponse;
+import com.devbarun.blog.domian.entities.Tag;
+import com.devbarun.blog.mappers.TagMapper;
+import com.devbarun.blog.services.TagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/api/v1/tags")
+@RequiredArgsConstructor
+public class TagController {
+
+    private final TagService tagService;
+    
+    private final TagMapper tagMapper;
+
+    @GetMapping
+    public ResponseEntity<List<TagResponse>> getAllTags(){
+        List<Tag> tags = tagService.getTags();
+        List<TagResponse> tagResponse= tags.stream().map(tagMapper::toTagResponse).toList();
+        return ResponseEntity.ok(tagResponse);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<TagResponse>> createTags(@RequestBody CreateTagsRequest createTagsRequest){
+        List<Tag> savedTags = tagService.createTags(createTagsRequest.getNames());
+        List<TagResponse> createdTagResponses = savedTags.stream().map(tagMapper::toTagResponse).toList();
+        return new ResponseEntity<>(
+                createdTagResponses,
+                HttpStatus.CREATED
+        );
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> deleteTag(@PathVariable UUID id){
+        tagService.deleteTag(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/devbarun/blog/services/TagService.java
@@ -0,0 +1,13 @@
+package com.devbarun.blog.services;
+
+import com.devbarun.blog.domian.entities.Tag;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+public interface TagService {
+    List<Tag> getTags();
+    List<Tag> createTags(Set<String> tagNames);
+    void deleteTag(UUID id);
+}
